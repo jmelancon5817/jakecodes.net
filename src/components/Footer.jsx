62ddba617c1f968ee617e25a8ccd74f2973d1863
Jacob Melancon <jmelancon5817@bellsouth.net>
2022-08-02 12:26:01 -0500

    Add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { FaGithub, FaTwitter, FaInstagram } from "react-icons/fa";
+
+const Footer = () => {
+  const links = [
+    {
+      id: 1,
+      child: (
+        <>
+          <FaTwitter size={30} />
+        </>
+      ),
+      href: "https://twitter.com",
+    },
+    {
+      id: 2,
+      child: (
+        <>
+          <FaGithub size={30} />
+        </>
+      ),
+      href: "https://github.com",
+    },
+    {
+      id: 3,
+      child: (
+        <>
+          <FaInstagram size={30} />
+        </>
+      ),
+      href: "https:instagram.com",
+    },
+  ];
+
+  return (
+    <div>
+      <div className="border-t-2 flex justify-center items-center w-full h-20 px-4 z-10 text-white bg-gray-900">
+        <ul className="flex">
+          {links.map(({ id, child, href }) => (
+            <li
+              key={id}
+              className="px-4 cursor-pointer font-medium text-gray-500 hover:scale-105 duration-200 hover:text-white"
+            >
+              <a href={href}>{child}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
